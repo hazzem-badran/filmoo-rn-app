4f6da4bacee755bab3fbba3d1697804aebd132a9
Hazzembadran <hazemaqbadran@gmail.com>
2025-05-08 15:21:22 +0300

    Refactor app structure: update app name, replace icons, remove unused screens, and enhance Home screen layout with new components.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,6 @@
-import "./globals.css";
+import './globals.css'
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen 
-        name="(tabs)"
-        options={{ headerShown: false }} 
-      />
-      <Stack.Screen 
-        name="movie/[id]"
-        options={{ headerShown: false }} 
-      />
-    </Stack>
-  );
+  return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
@@ -23,3 +21,4 @@ module.exports = {
   },
   plugins: [],
 }
+
